4e6a7b9f306d043c023a611087310d5a4d024249
cavalcantinanda <nandalima.cavalcanti@gmail.com>
2024-06-01 13:30:23 -0300

    meu projeto java

---
+++ conteudos-java/src/edu/fernanda/conteudosjava/MinhaClasse.java
@@ -0,0 +1,8 @@
+package edu.fernanda.conteudosjava;
+
+public class MinhaClasse {
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ conteudos-java/src/edu/fernanda/conteudosjava/Operadores.java
@@ -0,0 +1,5 @@
+package edu.fernanda.conteudosjava;
+
+public class Operadores {
+    
+}
